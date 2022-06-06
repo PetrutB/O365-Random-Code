2f6c6427ac62ba770c6c780513bfb54c1bcbc69f
Petrut Belingher <gpbelingher@yahoo.com>
2022-06-06 15:36:07 +0300

    Added a Sample of Call Records API Usage

---
+++ CallRecords -Sample.ps1
@@ -0,0 +1,11 @@
+﻿Install-Module -Name Microsoft.Graph.Teams
+Install-Module -Name Microsoft.Graph.CloudCommunications
+
+Import-Module Microsoft.Graph.Teams
+Import-Module Microsoft.Graph.CloudCommunications
+
+Connect-MgGraph -TenantId "TenantID" -ClientId "ClientID" -CertificateThumbprint "ThumbprintID"
+# Connect-MgGraph -Scopes "CallRecords.Read.All"
+
+$callRecordId = "CallRecordID"
+$myCall = Get-MgCommunicationCallRecordSession -CallRecordId $callRecordId
